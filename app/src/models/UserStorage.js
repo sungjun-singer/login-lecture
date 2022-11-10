486b2a92b3d87c11befa913628821059a22daba3
kimsungjun <kis07150@gmail.com>
2022-11-10 17:22:15 +0900

    fs시스템 모듈을 통한 로그인 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,20 @@
 "use strict"
 
+const fs = require('fs').promises;
+
 class UserStorage {
-    static #users = {
-        id : ['woorimIT', "김성준", "오주연"],
-        password : ["1234", "1234", "123456"],
-        name : ['우리밋', "기린", "찐빵"],
-    };
+    static #getUserInfo(data, id){
+        const users = JSON.parse(data);
+            const idx = users.id.indexOf(id);
+            const userInfo = Object.keys(users).reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {})
+        return userInfo;
+    }
 
     static getUsers(...fields){
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
            if(users.hasOwnProperty(field)){
                 newUsers[field] = users[field];
@@ -17,23 +23,21 @@ class UserStorage {
         }, {});
         return newUsers; 
     }
+    
     static getUserInfo(id){
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const userInfo = Object.keys(users).reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {})
-
-        return userInfo;
+        return fs.readFile('./src/database/users.json')
+        .then((data) => {
+            return this.#getUserInfo(data, id);
+        })
+        .catch(console.error);     
     }
-
+    
     static save(userInfo){
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.password.push(userInfo.password);
-        console.log(users);
+        return {success : true};
     }
 }
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -17,9 +17,9 @@ const output = {
 };
 
 const process = {
-    login : (req, res) => {
+    login : async(req, res) => {
         const user = new User(req.body);
-        const response = user.login();
+        const response = await user.login();
         return res.json(response);
     },
 
@@ -27,7 +27,7 @@ const process = {
         const user = new User(req.body);
         const response = user.register();
         return res.json(response);
-    }
+    },
 };
 
 
