afe97e9b5d14e03c82ab89ea883f3117c2a2ed73
kimsungjun <kis07150@gmail.com>
2022-11-10 09:48:00 +0900

    백엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const home = require('./src/routes/home');
 
@@ -9,6 +10,8 @@ const home = require('./src/routes/home');
 app.set('views', "./src/views");
 app.set('view engine', "ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.use('/', home); // use -> 미들 웨어를 등록해주는 메서드.
 
